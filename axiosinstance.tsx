e56a14bf28e606c3ce55d8034077fcd1788717a0
Arun <arunprajapati8502@gmail.com>
2024-04-04 17:56:12 +0530

    update baseurl

--- axiosinstance.tsx
+++ axiosinstance.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 axios.interceptors.request.use(
   function (config) {
-    config.baseURL = 'https://flightappbackend-k5rb.vercel.app/api/user';
+    config.baseURL = 'https://flightappbackend-ee0w.onrender.com/api/user';
 
     return config;
   },
