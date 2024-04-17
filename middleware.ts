3fc4f38f45918860e9ee7769b6f8cfdaf927dff9
Arun <arunprajapati8502@gmail.com>
2024-04-17 16:30:09 +0530

    cookies()

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,20 @@
 import { NextResponse, type NextRequest } from "next/server";
+import {cookies} from "next/headers";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
-  const currentUser = request.cookies.get("accessToken")?.value;
-  console.log(currentUser, "current user")
+  // const currentUser = request.cookies.get("accessToken")?.value;
+  const currentUserstore = cookies()
+  const currentUser =currentUserstore.get("accessToken")?.value;
+  console.log(currentUser, "current user",request.url)
+  console.log("nextUrl",request.nextUrl)
 
   // if (!currentUser && request.nextUrl.pathname.startsWith("/auth/login")) {
   //   return Response.redirect(new URL("/auth/login", request.url));
   // }
-  if (currentUser ) {
-    return Response.json({msg:'cookie not found'});
-  }
+  // if (currentUser ) {
+  //   return Response.json({msg:'cookie not found'});
+  // }
   if (currentUser && request.nextUrl.pathname.startsWith("/auth/login")) {
     return Response.redirect(new URL("/", request.url));
   }
