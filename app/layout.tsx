631d83c497a90c64cc5be247b37cd0060b070329
Arun <arunprajapati8502@gmail.com>
2024-05-01 15:44:36 +0530

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import { Providers } from "./providers";
 import { ToasterContext } from "@/context/HotToaster";
 const inter = Inter({ subsets: ["latin"] });
 import { cookies } from "next/headers";
