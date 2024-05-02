f495f15072e44c546f57690c0b1187012806e105
Arun <arunprajapati8502@gmail.com>
2024-05-02 12:18:47 +0530

    change basseurl

--- axiosinstance.tsx
+++ axiosinstance.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from 'axios';    
 const axiosInstance = axios.create({
-  // baseURL:  `https://flightappbackend-k5rb.vercel.app/api/user`,
+  baseURL:  `https://flightappbackend-k5rb.vercel.app/api/user`,
   // baseURL:  `${process.env.LOCAL_BACKEND_BASE_URL}/api/user`,
-  baseURL:  `http://localhost:5000/api/user`,
+  // baseURL:  `http://localhost:5000/api/user`,
 
   timeout:200000,
   withCredentials: true,
