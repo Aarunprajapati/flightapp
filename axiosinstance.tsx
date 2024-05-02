00509cbc081c89a594b826214c6d4f0ac2b64ccc
Arun <arunprajapati8502@gmail.com>
2024-05-02 13:15:56 +0530

    baseurl change

--- axiosinstance.tsx
+++ axiosinstance.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
 const axiosInstance = axios.create({
-  // baseURL:  process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
-  baseURL: process.env.NEXT_PUBLIC_LOCALBACKEND_URL,
+  baseURL:  process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+  // baseURL: process.env.NEXT_PUBLIC_LOCALBACKEND_URL,
 
   timeout: 200000,
   withCredentials: true,
