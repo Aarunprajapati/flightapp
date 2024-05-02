cf17ea2b39a579b30aedded8a43984c5c8dd3dc2
Arun <arunprajapati8502@gmail.com>
2024-05-02 13:10:21 +0530

    update some changes

--- axiosinstance.tsx
+++ axiosinstance.tsx
@@ -1,29 +1,15 @@
 /* eslint-disable import/no-anonymous-default-export */
-import axios from 'axios';    
+import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL:  `https://flightappbackend-k5rb.vercel.app/api/user`,
-  // baseURL:  `${process.env.LOCAL_BACKEND_BASE_URL}/api/user`,
-  // baseURL:  `http://localhost:5000/api/user`,
+  // baseURL:  process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_LOCALBACKEND_URL,
 
-  timeout:200000,
+  timeout: 200000,
   withCredentials: true,
-  
 });
-
-// axiosInstance.interceptors.request.use(
-//   function (config) {
-//     config.baseURL = 'http://localhost:5000/api/user';
-
-//     return config;
-//   },
-//   function (error) {
-//     return Promise.reject(error);
-//   }
-// );
-
 export default {
   get: axiosInstance.get,
   post: axiosInstance.post,
   put: axiosInstance.put,
   delete: axiosInstance.delete,
-}
+};
